7c9876b2bed328f850f333fa11154134e6d2bd44
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-10 22:56:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  kickstartercss(".clearfix list-simple, li").each { |e|  puts e}
+  kickstarter.css(".clearfix list-simple, li").each { |e|  puts e}
   # binding.pry
 end
