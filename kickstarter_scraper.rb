3d16a955d1355778a62076224c0b15d80497f12d
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-10 23:13:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,16 +6,18 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  result = {}
-  kickstarter.css(".clearfix list-simple, .project grid_4, h2").each do |e|
-    title = e.css("a").text
-    result[title] = {
-      :image_link => e.css("div.project-thumbnail a img").attribute,
-      :description => e.css("p.bbcard_blurb").text,
-      :location => e.css("ul.project-meta span.location-name").text,
-      :percent_funded => e.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
-  end
-  result
-  # binding.pry
+  projects = {}
+
+   kickstarter.css("li.project.grid_4").each do |project|
+     title = project.css("h2.bbcard_name strong a").text
+     projects[title.to_sym] = {
+       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+       :description => project.css("p.bbcard_blurb").text,
+       :location => project.css("ul.project-meta span.location-name").text,
+       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+     }
+   end
+
+   # return the projects hash
+   projects
 end
